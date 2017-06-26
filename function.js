e1724e2a8dcbfb1594084165810bd9fda839afe4
hikarinotomadoi <hikarinotomadoi@gmail.com>
2017-06-26 18:46:12 +0900

    Create function.js

---
+++ function.js
@@ -0,0 +1,10 @@
+var result; //結果を保存する変数
+function multiply(a,b,c){ //関数の定義
+  result=a*b*c*1000;  //処理を書く
+}
+multiply(1,2,4);//関数の実行
+
+function return_value(a,b,c,d){
+  return a*b*c*d; //値を返す
+}
+var result2=return_value(1,2,3,4);//関数の返り値を代入
